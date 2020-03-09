3d8eeba052011beee2b907e7888628b4d07e3ba5
Dominant7b9 <60897066+Dominant7b9@users.noreply.github.com>
2020-03-09 10:33:42 -0500

    final changes added yelp api and searchbar states

---
+++ src/util/Yelp.js
@@ -0,0 +1,31 @@
+const apiKey = "Bskvv29yMOhvo0n4dQq2kAd2VZg7begeTNHEh6DGUaywBRjumyZcgyd8_edn6jG5k1Pmz6c-4ztLEJTt3UOGxmBTNOG9ssKnvtORSvj2JHXYuOdrKn41rCmd7gxkXnYx";
+
+const Yelp = {
+    searchYelp(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        {headers: 
+            {Authorization: `Bearer ${apiKey}`}
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if(jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    };
+                });
+            }
+        });
+    }    
+}
+
+export default Yelp;
